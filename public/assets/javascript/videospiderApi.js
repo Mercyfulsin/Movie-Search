a72eff4d1c3db1354f45472e9cdbae0b21125f05
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-27 19:39:20 -0800

    Forgot some references to old IP API

--- public/assets/javascript/videospiderApi.js
+++ public/assets/javascript/videospiderApi.js
@@ -9,18 +9,18 @@ $(document).ready(function () {
 });
 
 function updateUrl() {
-    const obj = {movie: omdbId, ip: theIP};
+    const obj = { movie: omdbId };
     console.log("The obj", obj);
-    $.ajax({ url:"/api/spider/", type:'POST', data: obj})
-    .then(reply=>{
-        console.log("DB Reply", reply);
-        videoUrl = `https://videospider.stream/getvideo?key=XQfajbgr6UlJuc8S&video_id=${omdbId}&ticket=${reply}`;
-        getVideo();
-    }).catch(err=>console.log("err",err));
+    $.ajax({ url: "/api/spider/", type: 'POST', data: obj })
+        .then(reply => {
+            console.log("DB Reply", reply);
+            videoUrl = `https://videospider.stream/getvideo?key=XQfajbgr6UlJuc8S&video_id=${omdbId}&ticket=${reply}`;
+            getVideo();
+        }).catch(err => console.log("err", err));
 };
 
-        
+
 function getVideo() {
-    console.log("Hello",videoUrl);
-    $("#freelink").attr("src",videoUrl);
+    console.log("Hello", videoUrl);
+    $("#freelink").attr("src", videoUrl);
 }
