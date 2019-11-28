d915ab0eb36bba5cebe9214f238a58ed4ca2dcef
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-11-27 19:35:07 -0800

    Removed API for IP, using request header for IP

--- src/routes/api/videospider.js
+++ src/routes/api/videospider.js
@@ -4,9 +4,8 @@ import axios from 'axios';
 /* /api/spider */
 router.route('/')
     .post((req, res) => {
-        const { movie, ip } = req.body;
-        console.log("====================",req.connection);
-        console.log("********************", req.headers, req.headers['x-forwarded-for']);
+        const ip = req.headers['x-forwarded-for'];
+        const { movie } = req.body;
         const url = `https://videospider.in/getticket.php?key=XQfajbgr6UlJuc8S&secret_key=zpqjtddfngl4trolf6blfts956n6ll&video_id=${movie}&ip=${ip}`;
         console.log("Get URL: ", url);
         axios(url).then(data => { console.log("Reply: ", data.data); res.send(data.data); }).catch(err => console.log(err));
